1fc48aa65375fa19a5bff5ee8cd35b68f018341e
kostrov <kostrov@raec.su>
2025-03-24 15:23:05 +0300

    234

--- src/Differ.php
+++ src/Differ.php
@@ -2,66 +2,82 @@
 
 namespace Differ\Differ;
 
-use function Funct\Collection\sortBy;
-use function Funct\Collection\union;
-use function Differ\Parsers\parse;
-use function Differ\Formatters\format;
+use function Differ\Parser\parseFile;
 
-function readFile(string $filePath): string
+/**
+ * Generates the difference between two data structures.
+ *
+ * @param array $firstData The first data structure (associative array).
+ * @param array $secondData The second data structure (associative array).
+ *
+ * @return string The formatted difference as a string.
+ */
+function genDiff(array $firstData, array $secondData): string
 {
-    if (!file_exists($filePath)) {
-        throw new \Exception("The file {$filePath} does not exists.");
+    $diff = buildDiff($firstData, $secondData);
+    return formatDiff($diff);
+}
+
+/**
+ * Builds a diff array representing the differences between two data structures.
+ *
+ * @param array $firstData The first data structure (associative array).
+ * @param array $secondData The second data structure (associative array).
+ *
+ * @return array The diff array.
+ */
+function buildDiff(array $firstData, array $secondData): array
+{
+    $keys1 = array_keys($firstData);
+    $keys2 = array_keys($secondData);
+    $allKeys = array_unique(array_merge($keys1, $keys2));
+    sort($allKeys);
+
+    $diff = [];
+    foreach ($allKeys as $key) {
+        if (array_key_exists($key, $firstData) && array_key_exists($key, $secondData)) {
+            if ($firstData[$key] === $secondData[$key]) {
+                $diff[] = ['key' => $key, 'type' => 'unchanged', 'value' => $firstData[$key]];
+            } else {
+                $diff[] = ['key' => $key, 'type' => 'changed', 'oldValue' => $firstData[$key], 'newValue' => $secondData[$key]];
+            }
+        } elseif (array_key_exists($key, $firstData)) {
+            $diff[] = ['key' => $key, 'type' => 'removed', 'value' => $firstData[$key]];
+        } else {
+            $diff[] = ['key' => $key, 'type' => 'added', 'value' => $secondData[$key]];
+        }
     }
 
-    return (string) file_get_contents($filePath);
+    return $diff;
 }
 
-function genDiff(string $firstFilePath, string $secondFilePath, string $format = 'stylish'): string
+/**
+ * Formats the diff array into a string representation.
+ *
+ * @param array $diff The diff array.
+ *
+ * @return string The formatted diff string.
+ */
+function formatDiff(array $diff): string
 {
-    $firstData = readFile($firstFilePath);
-    $secondData = readFile($secondFilePath);
-
-    $parsedFirstData = parse($firstData, pathinfo($firstFilePath, PATHINFO_EXTENSION));
-    $parsedSecondData = parse($secondData, pathinfo($secondFilePath, PATHINFO_EXTENSION));
-
-    $ast = genAst($parsedFirstData, $parsedSecondData);
-
-    return format($ast, $format);
-}
-
-function genAst(object $firstData, object $secondData): array
-{
-    $firstKeys = array_keys(get_object_vars($firstData));
-    $secondKeys = array_keys(get_object_vars($secondData));
-    $unionKeys = union($firstKeys, $secondKeys);
-    $sortedKeys = array_values(sortBy($unionKeys, fn($key) => $key));
-
-    $buildAst = array_map(function ($key) use ($firstData, $secondData): array {
-        if (!property_exists($firstData, $key)) {
-            return makeNode($key, 'added', null, $secondData->$key);
+    $result = "{\n";
+    foreach ($diff as $item) {
+        switch ($item['type']) {
+            case 'unchanged':
+                $result .= "    {$item['key']}: {$item['value']}\n";
+                break;
+            case 'added':
+                $result .= "  + {$item['key']}: {$item['value']}\n";
+                break;
+            case 'removed':
+                $result .= "  - {$item['key']}: {$item['value']}\n";
+                break;
+            case 'changed':
+                $result .= "  - {$item['key']}: {$item['oldValue']}\n";
+                $result .= "  + {$item['key']}: {$item['newValue']}\n";
+                break;
         }
-        if (!property_exists($secondData, $key)) {
-            return makeNode($key, 'removed', $firstData->$key, null);
-        }
-        if (is_object($firstData->$key) && is_object($secondData->$key)) {
-            return makeNode($key, 'complex', null, null, genAst($firstData->$key, $secondData->$key));
-        }
-        if ($firstData->$key === $secondData->$key) {
-            return makeNode($key, 'unchanged', $firstData->$key, $secondData->$key);
-        }
-        return makeNode($key, 'updated', $firstData->$key, $secondData->$key);
-    }, $sortedKeys);
-
-    return $buildAst;
-}
-
-function makeNode(string $key, string $type, $oldValue, $newValue, $children = null): array
-{
-    return [
-        'key' => $key,
-        'type' => $type,
-        'oldValue' => $oldValue,
-        'newValue' => $newValue,
-        'children' => $children
-    ];
+    }
+    $result .= "}";
+    return $result;
 }
--- src/Parsers.php
+++ src/Parsers.php
@@ -2,35 +2,46 @@
 
 namespace Differ\Parser;
 
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Exception\ParseException;
+
+/**
+ * Parses a file and returns its content as an associative array.
+ *
+ * @param string $filePath The path to the file.
+ *
+ * @return array The content of the file as an associative array.
+ *
+ * @throws \Exception if the file format is not supported or the file does not exist.
+ */
 function parseFile(string $filePath): array
 {
     if (!file_exists($filePath)) {
-        throw new \Exception("Файл не найден: {$filePath}");
+        throw new \Exception("File not found: {$filePath}");
     }
 
+    $fileExtension = pathinfo($filePath, PATHINFO_EXTENSION);
     $fileContent = file_get_contents($filePath);
-
-    // Определите тип файла (по расширению или содержимому)
-    $fileExtension = strtolower(pathinfo($filePath, PATHINFO_EXTENSION));
 
     switch ($fileExtension) {
         case 'json':
             $data = json_decode($fileContent, true);
             if (json_last_error() !== JSON_ERROR_NONE) {
-                throw new \Exception("Ошибка при декодировании JSON: " . json_last_error_msg());
+                throw new \Exception("Error decoding JSON: " . json_last_error_msg());
             }
-            break;
-        // TODO:  Добавьте поддержку YAML при необходимости
-        // case 'yaml':
-        // case 'yml':
-        //     $data = yaml_parse($fileContent);
-        //     if ($data === false) {
-        //         throw new \Exception("Ошибка при парсинге YAML");
-        //     }
-        //     break;
+            return $data;
+        case 'yaml':
+        case 'yml':
+            try {
+                $data = Yaml::parse($fileContent);
+                if (!is_array($data)) {
+                    throw new \Exception("YAML file does not contain an array.");
+                }
+                return $data;
+            } catch (ParseException $exception) {
+                throw new \Exception("Error parsing YAML: " . $exception->getMessage());
+            }
         default:
-            throw new \Exception("Неподдерживаемый формат файла: {$fileExtension}");
+            throw new \Exception("Unsupported file format: {$fileExtension}");
     }
-
-    return $data;
 }
